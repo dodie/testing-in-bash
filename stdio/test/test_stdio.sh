26f04a2b0bb823cd42b584283083a808885560ec
Dávid Csákvári <dodiehun@gmail.com>
2020-05-09 20:59:04 +0200

    finish example

--- stdio/test/test_stdio.sh
+++ stdio/test/test_stdio.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SRC="$(dirname .)/../src"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SRC="${script_dir}/../src"
 
 echo "Test that the program takes user input and prints the result to the standard output (verifying multiple lines)"
 
-result=$(printf "John\n42\n" | ${SRC}/interactive_greeter.sh)
+result=$(printf "John\n42\n" | ${SRC}/interactive_greeter.sh 2>/dev/null)
 expected=$(printf "Hello, John!\nYou are 42 years old")
 
 if [[ "${result}" == "${expected}" ]]; then
@@ -16,4 +17,12 @@ fi
 
 echo "Test that the program ask questions via standard error"
 
+result=$(printf "Any\n12\n" | ${SRC}/interactive_greeter.sh 2>&1 >/dev/null)
+expected=$(printf "Please enter your name:\nPlease enter your age:")
 
+if [[ "${result}" == "${expected}" ]]; then
+  echo "Test passed!"
+else
+  echo "Test failed!"
+  exit 1
+fi
