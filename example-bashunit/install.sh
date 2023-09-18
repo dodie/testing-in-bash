d22cc4cd377f2fe7c5983f73c1b1ed9da1910ad5
katarn <katarn@katarn.es>
2023-09-18 20:43:36 +0200

    Add install script

---
+++ example-bashunit/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+rm -rf lib
+mkdir lib
+cd lib
+
+curl -L -O -J https://github.com/TypedDevs/bashunit/archive/refs/tags/0.5.0.tar.gz
+tar -zxvf bashunit-0.5.0.tar.gz
+mv bashunit-0.5.0 bashunit
+
