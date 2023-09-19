2231ff83b7bea55f70f1a227a260711c8415f187
katarn <katarn@katarn.es>
2023-09-19 18:39:49 +0200

    Update lib version

--- example-bashunit/install.sh
+++ example-bashunit/install.sh
@@ -5,7 +5,7 @@ rm -rf lib
 mkdir lib
 cd lib
 
-curl -L -O -J https://github.com/TypedDevs/bashunit/archive/refs/tags/0.5.0.tar.gz
-tar -zxvf bashunit-0.5.0.tar.gz
-mv bashunit-0.5.0 bashunit
+curl -L -O -J https://github.com/TypedDevs/bashunit/archive/refs/tags/0.6.0.tar.gz
+tar -zxvf bashunit-0.6.0.tar.gz
+mv bashunit-0.6.0 bashunit
 
