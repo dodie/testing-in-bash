1d03e49b933499955c5a55444735f86f9a428cd7
Chemaclass <chemaclass@outlook.es>
2023-09-30 13:49:19 +0200

    Invert install tag==main condition

--- example-bashunit/install.sh
+++ example-bashunit/install.sh
@@ -7,15 +7,15 @@ cd lib
 
 TAG=${1-main}
 
-if [[ $TAG != main ]]; then
+if [[ $TAG == main ]]; then
+  echo "> Using main branch"
+  git clone git@github.com:TypedDevs/bashunit.git temp_bashunit
+else
   echo "> Using a concrete tag '$TAG'"
   curl -L -O -J https://github.com/TypedDevs/bashunit/archive/refs/tags/0.7.0.tar.gz
   tar -zxvf bashunit-0.7.0.tar.gz
   cp bashunit-0.7.0/bin/bashunit temp_bashunit
   rm bashunit-0.7.0.tar.gz
-else
-  echo "> Using main branch"
-  git clone git@github.com:TypedDevs/bashunit.git temp_bashunit
 fi
 
 ## Common
