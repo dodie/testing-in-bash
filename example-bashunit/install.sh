29ae86a3cd502d17be247a7f31a49bdea3fe6cdf
Chemaclass <chemaclass@outlook.es>
2024-03-09 12:56:11 +0100

    install bashunit 0.11

--- example-bashunit/install.sh
+++ example-bashunit/install.sh
@@ -4,4 +4,4 @@
 # More details about bashunit installation         #
 # see: https://bashunit.typeddevs.com/installation #
 ####################################################
-curl -s https://bashunit.typeddevs.com/install.sh | bash -s lib 0.10.1
+curl -s https://bashunit.typeddevs.com/install.sh | bash -s lib 0.11.0
